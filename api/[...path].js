1811202c77551f891b7205a16347dc20643b7edd
unknown <ayazerguine.dev@gmail.com>
2026-01-28 21:41:07 +0100

    changed api function

--- api/[...path].js
+++ api/[...path].js
@@ -1,34 +1,24 @@
 import fetch from "node-fetch";
-import { Buffer } from "buffer";
-
-export const config = { api: { bodyParser: true, }, };
 
 export default async function handler(req, res) {
-  try {
-    const backend = "https://pinterest-backend-lvmx.onrender.com";
-    const url = backend + req.url.replace(/^\/api/, "");
-
-    const headers = { ...req.headers };
-    delete headers.host;
-    delete headers['content-length'];
-    const fetchOptions = {
-      method: req.method,
-      headers,
-      body: req.method !== "GET" && req.method !== "HEAD" ? JSON.stringify(req.body) : undefined, }
-
-    const response = await fetch(url, fetchOptions);
-    res.status(response.status);
-
-    response.headers.forEach((value, key) => {
-        if (!["content-length", "transfer-encoding", "transfer-encoding"].includes(key.toLowerCase())) {
-            res.setHeader(key, value);
-        }
-    });
-    const buffer = Buffer.from(await response.arrayBuffer());
-    res.send(buffer);
-
-  } catch (err) {
-    console.error("Proxy error:", err);
-    res.status(500).json({ error: "Proxy crashed" });
-  }
+    try {
+        const backend = "backend_URL";
+        const url = backend + req.url.replace(/^\/api/, "");
+        
+        const response = await fetch(url, {
+            method: req.method,
+            headers: { "Content-Type": "application/json", cookie: req.headers.cookie || "",},
+            body: req.method === "GET" || req.method === "HEAD" ? undefined : JSON.stringify(req.body), });
+            const text = await response.text();
+            
+            res.status(response.status);
+            res.setHeader("Content-Type", "application/json");
+            if (response.headers.get("set-cookie")) {
+                res.setHeader("Set-Cookie", response.headers.get("set-cookie"));
+            }
+            res.send(text);
+    } catch (err) {
+        console.error("Function error:", err);
+        res.status(500).json({ error: "Function crashed" });
+    }
 }
