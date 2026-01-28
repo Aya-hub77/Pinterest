aa7a342a813b8c9fa3fc04e70c4da01ae3c59f0b
unknown <ayazerguine.dev@gmail.com>
2026-01-28 21:29:17 +0100

    modified path.js

--- api/[...path].js
+++ api/[...path].js
@@ -1,22 +1,28 @@
 import fetch from "node-fetch";
 import { Buffer } from "buffer";
 
+export const config = { api: { bodyParser: true, }, };
+
 export default async function handler(req, res) {
   try {
     const backend = "https://pinterest-backend-lvmx.onrender.com";
     const url = backend + req.url.replace(/^\/api/, "");
 
-    const headers = { ...req.headers, host: undefined, };
+    const headers = { ...req.headers };
+    delete headers.host;
+    delete headers['content-length'];
     const fetchOptions = {
       method: req.method,
       headers,
-      body: req.method !== "GET" && req.method !== "HEAD" ? typeof req.body === "object" ? JSON.stringify(req.body) : req.body : undefined }
+      body: req.method !== "GET" && req.method !== "HEAD" ? JSON.stringify(req.body) : undefined, }
 
     const response = await fetch(url, fetchOptions);
     res.status(response.status);
 
     response.headers.forEach((value, key) => {
-      res.setHeader(key, value);
+        if (!["content-length", "transfer-encoding"].includes(key.toLowerCase())) {
+            res.setHeader(key, value);
+        }
     });
     const buffer = Buffer.from(await response.arrayBuffer());
     res.send(buffer);
